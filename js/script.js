e71b24aac926847d512f2237678ef814cb868daf
Marcin Węglorz <marcinweglorz.dev@gmail.com>
2023-01-26 23:18:20 +0100

    added current year function in js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 const nav = document.querySelector('.nav')
 const navBtn = document.querySelector('.burger-btn')
 const allNavItems = document.querySelectorAll('.nav__item')
+const navBtnBars = document.querySelector('.burger-btn__bars')
+const allSections = document.querySelectorAll('.section')
+const footerYear = document.querySelector('.footer__year')
 
 const handleClick = () => {
 	nav.classList.toggle('nav--active')
@@ -23,4 +26,10 @@ const handleNavItemsAnimations = () => {
 	})
 }
 
+const handleCurrentYear = () => {
+	const year = new Date().getFullYear()
+	footerYear.innerText = year
+}
+
+handleCurrentYear()
 navBtn.addEventListener('click', handleClick)
